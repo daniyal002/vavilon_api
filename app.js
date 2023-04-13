825c9bb941bff474c91b71abf345ce2ae699cfee
root <root@1403459-cy13881.tw1.ru>
2023-04-13 17:55:58 +0300

    Vavilon API  v_1.1

--- app.js
+++ app.js
@@ -40,6 +40,6 @@ sequelize
     console.error("Error synchronizing models with database:", err)
   );
 
-const port = 5001;
+const port = 5000;
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
