ff8cb2728bca52873bcaf2ba00783f81d99df83c
root <root@1403459-cy13881.tw1.ru>
2023-04-25 18:02:22 +0300

    Vavilon API v_2.3

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const express = require('express');
 const { Sequelize } = require('sequelize');
 const cors = require('cors');
 const fs = require('fs');
-const https = require('https');
-const io = require('socket.io');
-const WebSocket = require('ws')
 
 const moviesRouter = require('./routes/movies.js');
 const sessionRouter = require('./routes/sessions.js');
@@ -19,6 +16,7 @@ app.use(cors({
   origin: ['https://localhost:80', 'https://90.156.210.4:80', 'https://kinovavilon.ru'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // добавьте методы, используемые в вашем приложении
 }));
+
 
 app.use(express.json());
 app.use('/', moviesRouter, sessionRouter, orderRouter);
@@ -43,39 +41,16 @@ sequelize
     console.error('Error synchronizing models with database:', err)
   );
 
-const options = {
-  key: fs.readFileSync('/etc/letsencrypt/live/kinovavilon.ru/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/kinovavilon.ru/cert.pem')
-};
+  // const options = {
+  //   key: fs.readFileSync('/etc/letsencrypt/live/kinovavilon.ru/privkey.pem'),
+  //   cert: fs.readFileSync('/etc/letsencrypt/live/kinovavilon.ru/cert.pem')
+  // };
 
-// Создаем https сервер
-const server = https.createServer(options, app);
-
-const socketIo = io(server, {
-  cors: {
-    origin: ['https://localhost:3000', 'https://kinovavilon.ru'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials: true,
-  },
-});
-
-// Подключаемся к сокету
-socketIo.on('connection', (socket) => {
-  console.log('a user connected');
-
-  // Обрабатываем события от клиента
-  socket.on('eventFromClient', (data) => {
-    console.log(data);
-    io.emit('eventFromServer', 'Hello from server');
-  });
-
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-});
-
+   
 const port = 5000;
 
-server.listen(port, () => {
+
+ 
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
